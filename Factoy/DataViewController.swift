9220907f5e45ccda025c89710decf4954b99bd17
Mateo Došlić <mateo55@live.com>
2018-06-07 16:53:33 +0200

    Made Class of the preview, DatasourceDelegate,Tableview Delegate

--- Factoy/DataViewController.swift
+++ Factoy/DataViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DataViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
