76b8055beedda55dbed488da18ebabe318593be1
Mateo Došlić <mateo55@live.com>
2018-06-13 21:18:46 +0200

    Experiment.2

--- Factoy/PreviewDataTableViewController.swift
+++ Factoy/PreviewDataTableViewController.swift
@@ -10,22 +10,18 @@ import UIKit
 import Alamofire
 import AlamofireImage
 
-let url = "https://newsapi.org/v1/articles?apiKey=6946d0c07a1c4555a4186bfcade76398&sortBy=top&source=bbc-news"
-
-
 class PreviewDataTableViewController: UITableViewController {
 
     
     // MARK: Properties
     
-    var previewData = [Preview]()
-    var picture: UIImage?
+    var newsData: [Articles] = []
  
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Load  peview
-        LoadPreview()
+        //Load News
+        LoadNews()
 
     }
 
@@ -91,66 +87,24 @@ class PreviewDataTableViewController: UITableViewController {
     }
 
     // MARK: Function for loading Data from URL
-    private func LoadPreview() {
-        
-        let utilityQueue = DispatchQueue.global(qos: .utility)
-        // Validating URL response
-        Alamofire.request(url)
-            .validate()
-
-            
-            
-            
-            
-            
-            .responseJSON { response in
-            
-            switch response.result {
-                
-            case .success:
-                print("Validation Successful")
-               
-                
-                // Parsing data
-                let JSON = response.result.value as! [String: Any]
-                let JSONArticles = JSON["articles"] as! NSArray
-                for Articles in JSONArticles {
+    private func LoadNews() {
+        let url = URL(string: "https://newsapi.org/v1/articles?apiKey=6946d0c07a1c4555a4186bfcade76398&sortBy=top&source=bbc-news")
+        Alamofire.request(url!).responseJSON { (response) in
+            let result = response.data
+            do {
+                let data = try JSONDecoder().decode(News.self, from: result!)
+                self.newsData = data.articles
+                DispatchQueue.main.async {
                     
-                    // Saving important values
-                    var Values = Articles as! [String: String]
-                    let headline = Values["title"] as! String
-                    let photo_string = Values["urlToImage"] as! String
-                    var story = Values["description"] as! String
-                    story += "\n"
-                    story += Values["url"] as! String
-
-                    let photoURL = URL(string: photo_string)
                     
-                    Alamofire.request(photoURL!)
-                        .downloadProgress(queue: utilityQueue, closure: { (progress) in
-                            print("Downloaded: \(progress.fractionCompleted)")
-                        })
-                        
-                        .responseImage { response in
-                        
-                        if let image = response.result.value {
-                         
-                            // Saving Values in preview Object
-                            guard let previewx = Preview(headline: headline, photo: image, story: story)
-                                else {
-                                    fatalError("Unable to instantianite preview")
-                            }
-                            self.previewData += [previewx]
-                            DispatchQueue.main.async{
-                                self.tableView.reloadData()
-                            }
-                        }
-                    }
+                    self.tableView.reloadData()
                 }
-
-            case .failure(let error):
-                print(error)
+            }catch {
+                print("error")
             }
+            
         }
+        
+        
     }
 }
